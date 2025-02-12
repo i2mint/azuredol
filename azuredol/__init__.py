d5e63ad912f39321ef598dd11ae33328924cd469
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2025-02-12 09:12:33 -0800

    feat: Refactor to AzureFiles

--- azuredol/__init__.py
+++ azuredol/__init__.py
@@ -1,3 +1,9 @@
 """Access Azure Storage through a mapping interface"""
 
-from azuredol.base import AzureBlobStore
+from azuredol.base import (
+    AzureFiles,
+    AzureReader,
+    AzureTextFiles,
+)
+
+from azuredol._old_base import AzureBlobStore  # deprecated
--- azuredol/base.py
+++ azuredol/base.py
@@ -1,7 +1,14 @@
 """Azure Storage Data Object Layer"""
+
 from dataclasses import dataclass
 from azure.storage.blob import ContainerClient
-from dol import KvReader, KvPersister
+from dol import KvReader, KvPersister, filt_iter, wrap_kvs, ValueCodecs
+
+# TODO: Implement three types of blobs: block, append, and page blobs (see https://github.com/i2mint/azuredol/discussions/2#discussioncomment-12176104)
+# TODO: Use cache_property to cache the container client
+# TODO: Implement appendable blob objects
+# TODO: Implement close to metal len and contains methods
+# TODO: Do we really need/want the key codecs in the base class?
 
 AZURE_STORAGE_BLOCK_SIZE_LIMIT = 4 * 1024 * 1024  # 4MB
 
@@ -14,46 +21,41 @@ def _append_block(blob_client, v):
             blob_client.append_block(block, length=len(block))
 
 
-class AzureBlobPersisterMixin(KvPersister):
-    """Key-Value mapping for creating, updating, and deleting Azure storage blob data"""
+class AzureReader(KvReader):
+    """
+    Key-Value mapping for accessing and reading Azure storage blob data
+    """
 
-    def __setitem__(self, k, v):
-        """Create appendable blob and set value
+    def __init__(
+        self,
+        container_name,
+        path: str = None,
+        *,
+        connection_string: str,
+        create_container_if_missing=False,
+    ):
+        """Connect to Azure storate blob service client to create and access appendable
+        blob data"""
+        self._init_kwargs = dict(
+            container_name=container_name,
+            path=path,
+            connection_string=connection_string,
+            create_container_if_missing=create_container_if_missing,
+        )
+        self.container_name = container_name
+        self._container_client = ContainerClient.from_connection_string(
+            connection_string, self.container_name
+        )
+        if not self._container_client.exists():
+            if self.create_container_if_missing:
+                self._container_client.create_container()
+            else:
+                raise ValueError(f'Container {self.container_name} does not exist')
+        self.path = f"{path.strip('/')}/" if path else ''
+        self.create_container_if_missing = create_container_if_missing
 
-        :param k: key
-        :param v: blob data
-        :return: None
-        """
-        blob_client = self._container_client.get_blob_client(blob=self._id_of_key(k))
-        blob_client.create_append_blob()
-        _append_block(blob_client, v)
-
-    # TODO: Would be nice to have store[k].append(v) instead of this. The hard part is
-    # that we don't want to download the blob when calling store[k] in this case.
-    def append_to_value(self, k, v):
-        """Append to existing block
-
-        :param k: key
-        :param v: blob data
-        :return: None
-        """
-        blob_client = self._container_client.get_blob_client(blob=self._id_of_key(k))
-        if not blob_client.exists():
-            raise KeyError(k)
-        _append_block(blob_client, v)
-
-    def __delitem__(self, k):
-        """Delete blob
-
-        :param k: key
-        :return: None
-        """
-        blob_client = self._container_client.get_blob_client(blob=self._id_of_key(k))
-        blob_client.delete_blob()
-
-
-class AzureBlobReaderMixin(KvReader):
-    """Key-Value mapping for accessing and reading Azure storage blob data"""
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.container_name!r}, path={self.path!r})'
 
     def __getitem__(self, k):
         """Download and return blob data
@@ -61,7 +63,11 @@ class AzureBlobReaderMixin(KvReader):
         :param k: key
         :return: blob data
         """
-        blob_client = self._container_client.get_blob_client(blob=self._id_of_key(k))
+        _id = self._id_of_key(k)
+        if _id.endswith('/'):
+            _new_init_kwargs = dict(self._init_kwargs, path=_id)
+            return self.__class__(**_new_init_kwargs)
+        blob_client = self._container_client.get_blob_client(blob=_id)
         blob_data = blob_client.download_blob().readall()
         return blob_data
 
@@ -73,17 +79,39 @@ class AzureBlobReaderMixin(KvReader):
         blob_iter = self._container_client.list_blobs(self.path)
         return (self._key_of_id(blob.name) for blob in blob_iter)
 
+    def __contains__(self, k):
+        """Check if blob exists
 
-@dataclass
-class AzureBlobStore(AzureBlobReaderMixin, AzureBlobPersisterMixin):
-    """Azure storage blob data key-value mapping for creating, reading, updating, and 
+        :param k: key
+        :return: True if blob exists, False otherwise
+        """
+        blob_client = self._container_client.get_blob_client(blob=self._id_of_key(k))
+        return blob_client.exists()
+
+    def __len__(self):
+        """Count the number of blobs
+
+        :return: Number of blobs
+        """
+        return sum(1 for _ in self)
+
+    def _id_of_key(self, k):
+        return f'{self.path}{k}'
+
+    def _key_of_id(self, id):
+        return id[len(self.path) :]
+
+
+class AzureFiles(AzureReader, KvPersister):
+    """
+    Azure storage blob data key-value mapping for creating, reading, updating, and
     deleting
 
     :param container_name: unique identifier used to distinguish container instances
-    :param connection_string: authorization info to access data in an Azure Storage 
+    :param connection_string: authorization info to access data in an Azure Storage
     account
 
-    Let's use the local Azure Storage Emulator to test this out. Make sure you have the 
+    Let's use the local Azure Storage Emulator to test this out. Make sure you have the
     emulator installed and running.
 
     >>> connection_string='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;'
@@ -126,7 +154,7 @@ class AzureBlobStore(AzureBlobReaderMixin, AzureBlobPersisterMixin):
     >>> list(store.items())
     [('k1', b'v1v1'), ('k2', b'v2')]
 
-    Let's create another store instance without a path. We can see that the key is 
+    Let's create another store instance without a path. We can see that the key is
     present in this store as well:
 
     >>> store_all = AzureBlobStore(connection_string, container_name)
@@ -144,8 +172,8 @@ class AzureBlobStore(AzureBlobReaderMixin, AzureBlobPersisterMixin):
     >>> list(store_all)
     ['some/path/k2']
 
-    We can create another store instance with a different path and add a blob. We can 
-    see that the blob is present in this store but not the first one. Also, the blob is 
+    We can create another store instance with a different path and add a blob. We can
+    see that the blob is present in this store but not the first one. Also, the blob is
     present in the store without a path:
 
     >>> other_store = AzureBlobStore(connection_string, container_name, path='some/other/path')
@@ -157,7 +185,7 @@ class AzureBlobStore(AzureBlobReaderMixin, AzureBlobPersisterMixin):
     >>> list(store_all)
     ['some/other/path/k1', 'some/path/k2']
 
-    But, we can create a last store with a path that is the common prefix of the other 
+    But, we can create a last store with a path that is the common prefix of the other
     two paths. And we can see that both blobs are present in this store:
 
     >>> a_last_store = AzureBlobStore(connection_string, container_name, path='some')
@@ -165,32 +193,46 @@ class AzureBlobStore(AzureBlobReaderMixin, AzureBlobPersisterMixin):
     ['other/path/k1', 'path/k2']
 
     Let's delete all the blobs, to keep the Azure Storage Emulator clean:
-    
+
     >>> for k in store_all:
     ...     del store_all[k]
-    
+
     """
 
-    connection_string: str
-    container_name: str
-    create_container_if_missing: bool = True
-    path: str = None
+    def __setitem__(self, k, v):
+        """Create appendable blob and set value
 
-    def __post_init__(self):
-        """Connect to Azure storate blob service client to create and access appendable 
-        blob data"""
-        self._container_client = ContainerClient.from_connection_string(
-            self.connection_string, self.container_name
-        )
-        if not self._container_client.exists():
-            if self.create_container_if_missing:
-                self._container_client.create_container()
-            else:
-                raise ValueError(f'Container {self.container_name} does not exist')
-        self.path = f"{self.path.strip('/')}/" if self.path else ''
+        :param k: key
+        :param v: blob data
+        :return: None
+        """
+        blob_client = self._container_client.get_blob_client(blob=self._id_of_key(k))
+        blob_client.create_append_blob()
+        _append_block(blob_client, v)
 
-    def _id_of_key(self, k):
-        return f'{self.path}{k}'
+    # TODO: Would be nice to have store[k].append(v) instead of this. The hard part is
+    # that we don't want to download the blob when calling store[k] in this case.
+    def append_to_value(self, k, v):
+        """Append to existing block
 
-    def _key_of_id(self, id):
-        return id[len(self.path) :]
+        :param k: key
+        :param v: blob data
+        :return: None
+        """
+        blob_client = self._container_client.get_blob_client(blob=self._id_of_key(k))
+        if not blob_client.exists():
+            raise KeyError(k)
+        _append_block(blob_client, v)
+
+    def __delitem__(self, k):
+        """Delete blob
+
+        :param k: key
+        :return: None
+        """
+        blob_client = self._container_client.get_blob_client(blob=self._id_of_key(k))
+        blob_client.delete_blob()
+
+
+# TODO: Replace decoder by pickle-able one
+AzureTextFiles = wrap_kvs(AzureFiles, value_decoder=lambda x: x.decode())
